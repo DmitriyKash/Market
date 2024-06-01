d2fd334ca9f887555560b8eca16adb2380184480
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-06-01 19:15:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TelegramAuth from './components/TelegramAuth';
 import ProductList from './components/ProductList';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/auth" component={TelegramAuth} />
-        <Route path="/products" component={ProductList} />
-        <Route path="/" component={TelegramAuth} />
-      </Switch>
+      <Routes>
+        <Route path="/auth" element={<TelegramAuth />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/" element={<TelegramAuth />} />
+      </Routes>
     </Router>
   );
 };
