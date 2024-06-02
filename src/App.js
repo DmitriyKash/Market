678f6f27677c4138a014c433e2185fd4ec99985c
Dmiriy <152209418+DmitriyKash@users.noreply.github.com>
2024-06-02 10:47:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TelegramAuth from './components/TelegramAuth';
 import ProductList from './components/ProductList';
 import UserProfile from './components/UserProfile';
 
 const App = () => {
+  useEffect(() => {
+    window.Telegram.WebApp.expand();
+    window.Telegram.WebApp.ready();
+    
+    // Пример приветствия пользователя
+    const user = window.Telegram.WebApp.initDataUnsafe.user;
+    if (user) {
+      alert(`Добро пожаловать, @${user.username}!`);
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
